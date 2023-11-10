814af8ac85a569a7523557ef959149a8c8b517e9
user <user@DESKTOP-MH60O7N>
2023-11-10 22:27:39 +0800

    exercise 1 file

--- src/q1ex_ele_gatchalianjm/Q1EX_ELE_GatchalianJM.java
+++ src/q1ex_ele_gatchalianjm/Q1EX_ELE_GatchalianJM.java
@@ -14,7 +14,42 @@ public class Q1EX_ELE_GatchalianJM {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("main application !!");
+        String item1 = "Monitor";
+        String size1 = "large";
+        int buttons1 = 5;
+        
+        String item2 = "Keyboard";
+        String size2 = "medium";
+        int buttons2 = 106;
+        
+        String item3 = "Mouse";
+        String size3 = "small";
+        int buttons3 = 3;
+        
+        System.out.println("Item 1");
+        System.out.println("Name: " + item1);
+        System.out.println("Size: " + size1);
+        System.out.println("Number of Buttons: " + buttons1);
+        System.out.println("");
+        
+        System.out.println("Item 2");
+        System.out.println("Name: " + item2);
+        System.out.println("Size: " + size2);
+        System.out.println("Number of Buttons: " + buttons2);
+        System.out.println("");
+        
+        System.out.println("Item 3");
+        System.out.println("Name: " + item3);
+        System.out.println("Size: " + size3);
+        System.out.println("Number of Buttons: " + buttons3);
+        System.out.println("");
+        
+        boolean sizeCheck = size1.equals(size2)&&size2.equals(size3)&&size3.equals(size1);
+        System.out.println("Any of the items have the same sizes: " + sizeCheck);
+        int totalButtons = buttons1 + buttons2 + buttons3;
+        System.out.println("Total Buttons: " + totalButtons);
+        boolean buttsCheck = buttons1 + buttons3 > buttons2;
+        System.out.println("Monitor and mouse have more buttons combined than Keyboard: " + buttsCheck);
     }
     
 }
